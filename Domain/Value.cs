e96db18a7bccd5cd25f100ece95762e82b4605a2
Nathan Xu <nathan.xu@safecode.com.au>
2020-03-02 11:11:25 +1100

    End of section 4

--- Domain/Value.cs
+++ Domain/Value.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace Domain {
-    public class Value {
+﻿namespace Domain
+{
+    public class Value
+    {
         public int Id { get; set; }
         public string Name { get; set; }
     }

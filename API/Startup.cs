2c858ad8545b5303ea609540b919538d174b6fe3
Nan Xu <nathan.nanxu@gmail.com>
2020-04-01 17:35:36 +1100

    End of section 14 relationships

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,85 +18,98 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
+using AutoMapper;
 
 namespace API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup (IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services)
-        {
-            services.AddDbContext<DataContext> (opt =>
-            {
-                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
-            });
-            services.AddCors (opt =>
-            {
-                opt.AddPolicy ("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
-                });
-            });
-            services.AddMediatR (typeof (List.Handler).Assembly);
-            services.AddControllers (opt =>
-            {
-                var policy = new AuthorizationPolicyBuilder ().RequireAuthenticatedUser ().Build ();
-                opt.Filters.Add (new AuthorizeFilter (policy));
-            }).AddFluentValidation (cfg =>
-            {
-                cfg.RegisterValidatorsFromAssemblyContaining<Create> ();
-            });
-
-            var builder = services.AddIdentityCore<AppUser> ();
-            var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
-            identityBuilder.AddEntityFrameworkStores<DataContext> ();
-            identityBuilder.AddSignInManager<SignInManager<AppUser>> ();
-
-            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration["Tokenkey"]));
-
-            services.AddScoped<IJwtGenerator, JwtGenerator> ();
-            services.AddScoped<IUserAccessor, UserAccessor> ();
-
-            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).AddJwtBearer (opt =>
-            {
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = key,
-                ValidateAudience = false,
-                ValidateIssuer = false
-                };
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ErrorHandlingMiddleware> ();
-            if (env.IsDevelopment ())
-            {
-                // app.UseDeveloperExceptionPage ();
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseRouting ();
-            app.UseCors ("CorsPolicy");
-
-            app.UseAuthentication ();
-            app.UseAuthorization ();
-
-            app.UseEndpoints (endpoints =>
-            {
-                endpoints.MapControllers ();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt =>
+     {
+       opt.UseLazyLoadingProxies();
+       opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+     });
+      services.AddCors(opt =>
+     {
+       opt.AddPolicy("CorsPolicy", policy =>
+             {
+               policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+             });
+     });
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddAutoMapper(typeof(List.Handler));
+      services.AddControllers(opt =>
+     {
+       var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+       opt.Filters.Add(new AuthorizeFilter(policy));
+     }).AddFluentValidation(cfg =>
+           {
+             cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+           });
+
+      var builder = services.AddIdentityCore<AppUser>();
+      var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+      identityBuilder.AddEntityFrameworkStores<DataContext>();
+      identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+      services.AddAuthorization(opt =>
+      {
+        opt.AddPolicy("IsActivityHost", policy =>
+        {
+          policy.Requirements.Add(new IsHostRequirement());
+        });
+      });
+
+      services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokenkey"]));
+
+      services.AddScoped<IJwtGenerator, JwtGenerator>();
+      services.AddScoped<IUserAccessor, UserAccessor>();
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+    {
+      opt.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ValidateAudience = false,
+        ValidateIssuer = false
+      };
+    });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<ErrorHandlingMiddleware>();
+      if (env.IsDevelopment())
+      {
+        // app.UseDeveloperExceptionPage ();
+      }
+
+      // app.UseHttpsRedirection();
+
+      app.UseRouting();
+      app.UseCors("CorsPolicy");
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+     {
+       endpoints.MapControllers();
+     });
+    }
+  }
 }
---
+++ Application/Activities/AttendeeDTO.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Application.Activities
+{
+  public class AttendeeDTO
+  {
+    public string Username { get; set; }
+    public string DisplayName { get; set; }
+    public string Image { get; set; }
+    public bool isHost { get; set; }
+  }
+}

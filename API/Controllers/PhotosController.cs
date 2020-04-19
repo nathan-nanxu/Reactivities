0d17d38f2e716b59a2fea10c516fd7c62cd5249a
Nan Xu <nathan.nanxu@gmail.com>
2020-04-19 21:06:09 +1000

    End of section 17 upload photo client side

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -6,24 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class PhotosController : BaseController
+  public class PhotosController : BaseController
+  {
+    [HttpPost]
+    public async Task<ActionResult<Photo>> Add([FromForm] Add.Command command)
     {
-        [HttpPost]
-        public async Task<ActionResult<Photo>> Add ([FromForm] Add.Command command)
-        {
-            return await Mediator.Send (command);
-        }
-
-        [HttpDelete ("{id}")]
-        public async Task<ActionResult<Unit>> Delete (string id)
-        {
-            return await Mediator.Send (new Delete.Command { Id = id });
-        }
-
-        [HttpPut ("{id}/setmain")]
-        public async Task<ActionResult<Unit>> SetMain (string id)
-        {
-            return await Mediator.Send (new SetMain.Command { Id = id });
-        }
+      return await Mediator.Send(command);
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Unit>> Delete(string id)
+    {
+      return await Mediator.Send(new Delete.Command { Id = id });
+    }
+
+    [HttpPost("{id}/setmain")]
+    public async Task<ActionResult<Unit>> SetMain(string id)
+    {
+      return await Mediator.Send(new SetMain.Command { Id = id });
+    }
+  }
 }
